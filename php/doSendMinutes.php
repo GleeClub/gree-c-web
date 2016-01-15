90666abf137ded99f51d9efd7e6c9fb31df12942
Matthew Schauer <matthew.schauer.x@gmail.com>
2016-01-15 13:29:15 -0500

    Fixed minutes email to check that it's not being called spuriously

--- php/doSendMinutes.php
+++ php/doSendMinutes.php
@@ -1,6 +1,5 @@
 <?php
 require_once('functions.php');
-$id = mysql_real_escape_string($_POST['id']);
 $recipient = 'gleeclub_officers@lists.gatech.edu';
 $prefix = "<html><head></head><body>";
 $suffix = "</body></html>";
@@ -9,9 +8,12 @@ $headers = 'Content-type:text/html; charset=utf-8' . "\n" .
 	'From: Glee Club Officers <gleeclub_officers@lists.gatech.edu>' . "\n" .
 	'X-Mailer: PHP/' . phpversion();
 
+if (! isset($_POST['id'])) die("No ID specified");
+$id = mysql_real_escape_string($_POST['id']);
 $query = mysql_query("select `name`, `private` from `minutes` where `id` = '$id'");
 if (! $query) die("Query failed: " + mysql_error());
 $result = mysql_fetch_array($query);
+if (! $result) die("Failed to fetch minutes with that ID");
 $message = $prefix . $result['private'] . "<br><br>View these minutes online at http://gleeclub.gatech.edu/buzz/#minutes:$id<br>" . $suffix;
 $subject = "Minutes for " . $result['name'];
 
