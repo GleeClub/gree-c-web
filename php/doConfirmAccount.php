087fdcc943c762898f39c9280917270715e5cad1
Matthew Schauer <matthew.schauer@e10x.net>
2018-08-20 17:34:11 -0400

    Quick fix for taking defaultAttend into account when confirming an account

--- php/doConfirmAccount.php
+++ php/doConfirmAccount.php
@@ -10,6 +10,6 @@ $sect = mysql_real_escape_string($_POST['section']);
 if ($reg != "class" && $reg != "club") die("Invalid registration \"$reg\"");
 if (! mysql_query("insert into `activeSemester` (`member`, `semester`, `choir`, `enrollment`, `section`) values ('$USER', '$SEMESTER', '$CHOIR', '$reg', '$sect')")) die("Error confirming member: " . mysql_error());
 if (! mysql_query("update `member` set `location` = '$loc' where `email` = '$USER'")) die("Error setting location: " . mysql_error());
-if (! mysql_query("insert ignore into `attends` (`memberID`, `eventNo`) select '$USER', `eventNo` from `event` where `semester` = '$SEMESTER' and `choir` = '$CHOIR' and (`type` != 'sectional' or `section` = $sect or `section` = 0)")) die("Error setting attendance relationships: " . mysql_error());
+if (! mysql_query("insert ignore into `attends` (`memberID`, `eventNo`, `shouldAttend`) select '$USER', `eventNo`, `defaultAttend` from `event` where `semester` = '$SEMESTER' and `choir` = '$CHOIR' and (`type` != 'sectional' or `section` = $sect or `section` = 0)")) die("Error setting attendance relationships: " . mysql_error());
 echo "OK";
 ?>
